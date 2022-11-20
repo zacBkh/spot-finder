673de8c19c33f8cd2642529f977f9a17b586a58f
Zach <zachariedupain@hotmail.fr01~>
2022-11-20 19:15:06 +0400

    fix: cleaned up register & signin components

--- pages/auth/SignIn.jsx
+++ pages/auth/SignIn.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react"
 import { getProviders } from "next-auth/react"
 
-import { useRouter } from 'next/router'
 
 
 import LoginOrRegisterForm from "../../components/Forms/LoginOrRegisterForm"
@@ -33,87 +31,6 @@ export const getServerSideProps = async (context) => {
 // Component
 // Iterate over provider and display provider name
 const SignIn = ({ }) => {
-
-
-
-    // Store login failure/success info
-    const [loginStatus, setloginStatus] = useState(null);
-
-    // For toggler password visible
-    const [isPwdVisible, setIsPwdVisible] = useState(false);
-
-
-
-    // Yup stuff
-
-    // Yup Validation Schema
-    // const validationSchemaYup = Yup.object().shape({
-    //     email: Yup
-    //         .string().trim().lowercase()
-    //         .required("Email is required")
-    //         .email('It seems like this is not an email...'),
-
-
-    //     password: Yup
-    //         .string().trim()
-    //         .required("Password is required"),
-    // });
-
-
-
-
-    // // Formik stuff 
-
-    // const initialValues = {
-    //     email: "",
-    //     password: ""
-    // };
-
-
-
-
-    // // Formik - Submit Fx 
-    // const onSubmitFormik = async (formValues) => {
-
-    //     // https://next-auth.js.org/v3/getting-started/client#using-the-redirect-false-option
-    //     const loginResult = await signIn(
-    //         "credentials",
-    //         {
-    //             ...formValues,
-    //             redirect: false,
-
-    //         }
-    //     )
-
-    //     console.log('loginResult', loginResult)
-
-    //     // if auth issue linked to creds...
-    //     if (!loginResult.ok && loginResult.error === "CredentialsSignin") {
-    //         // router.push(`/auth/error?error=${loginResult.error}`)
-    //         setloginStatus("There has been an error verifying your credentials")
-
-    //         // if auth OK...
-    //     } else {
-    //         router.push(`/spots/allSpots`)
-    //     }
-    // }
-
-    // // Formik - object that tells initial values of form + submit & valid fx
-    // const formik = useFormik({
-    //     initialValues: initialValues,
-    //     onSubmit: onSubmitFormik,
-    //     validationSchema: validationSchemaYup
-    // })
-
-
-
-    // // Logic to deicde to display erorrMsg or not (depending of if mistake and if touched before)
-    // const showValidErrorMsg = (field) => {
-    //     // console.log('field', field)
-    //     if (formik.errors[field] && formik.touched[field]) {
-    //         return <span className="text-red-600">{formik.errors[field]}</span>
-    //     }
-    // }
 
 
 
