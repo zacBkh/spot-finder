8679e169f853de95be6e8560c17c8840b99f5a46
Zach <zachariedupain@hotmail.fr01~>
2022-12-01 10:46:35 +0400

    fix: added progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,40 @@ import Layout from '../components/Layout/Layout'
 import { SessionProvider } from "next-auth/react"
 
 
+// For loading bar
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import NProgress from 'nprogress'
+import '../styles/nprogress.css'
+
+
+
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
+
+
+
+  const router = useRouter()
+
+  // nProgress Bar
+  useEffect(() => {
+    // Tells NProgress to start/stop depending on the router state
+    router.events.on('routeChangeStart', () => NProgress.start())
+    router.events.on('routeChangeComplete', () => NProgress.done())
+    router.events.on('routeChangeError', () => NProgress.done())
+
+
+    // // Clean up fx ?
+    return () => {
+      router.events.off('routeChangeStart', NProgress.start())
+      router.events.off('routeChangeComplete', NProgress.done())
+      router.events.off('routeChangeError', NProgress.done())
+    }
+
+  }, [router])
+
+
+
 
   return (
     <SessionProvider session={session}>
