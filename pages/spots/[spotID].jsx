14de986d1a5a306e5e7a11b0c2cb08a410e91516
Zach <zachariedupain@hotmail.fr01~>
2022-12-03 18:55:59 +0400

    feat: started Modal for reviews

--- pages/spots/[spotID].jsx
+++ pages/spots/[spotID].jsx
@@ -25,7 +25,14 @@ import MapShow from '../../components/Maps/MapShow';
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
+
+
+import Reviews from '../../components/Reviews/Reviews';
+import ModalReviews from '../../components/Reviews/ModalReviews';
 import { Rating } from '@mui/material';
+
+
+
 
 export const getServerSideProps = async (context) => {
 
@@ -73,9 +80,16 @@ const ShowSpot = ({ indivSpot, currentUserID }) => {
     const [nbOfVisit, setNbOfVisit] = useState(nbVisit);
 
 
-
     // For star rating
     const [value, setValue] = useState(2);
+
+
+    // Review modal
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const onModalClose = () => {
+        setIsModalOpen(false)
+    }
 
 
     // To tell to API route which spot are we talking about -- can replace by info coming from GSP ? 
@@ -218,21 +232,53 @@ const ShowSpot = ({ indivSpot, currentUserID }) => {
             <p>LONGITUDE: {indivSpot.locationDrag.Longitude}</p>
 
 
-
-            <div>
-                <Rating
-                    name="simple-controlled"
-                    value={value}
-                    onChange={(event, newValue) => {
-                        setValue(newValue);
-                    }}
-                />
-            </div>
+            <a className='cursor-pointer'
+                onClick={() => setIsModalOpen(true)} >REVIEW THE SPOT
+            </a>
 
 
-            <div>
-                <Rating name="read-only" value={value} readOnly />
-            </div>
+            <Reviews
+                value={value}
+                isModalOpen={isModalOpen}
+                onModalClose={onModalClose}>
+
+
+                <ModalReviews
+                    isModalOpen={isModalOpen}
+                    onModalClose={onModalClose}>
+
+                    <div>
+                        <Rating
+                            name="simple-controlled"
+                            value={value}
+                            onChange={(event, newValue) => {
+                                setValue(newValue);
+                            }}
+                        />
+                    </div>
+
+
+                    <div>
+                        <Rating name="read-only" value={value} readOnly />
+                    </div>
+
+
+                    <form>
+                        {/* Text Area */}
+                        <div class="w-full  border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                            <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+                                <label for="comment" class="sr-only">Your comment</label>
+                                <textarea id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required></textarea>
+                            </div>
+
+
+
+
+
+                        </div>
+                    </form>
+                </ModalReviews>
+            </Reviews>
 
 
 
