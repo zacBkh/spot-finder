65441f82fd5759ccfd4ec868dbfb2c24a0d748bb
Zach <zachariedupain@hotmail.fr01~>
2023-01-09 07:59:08 +0100

    prettier

--- utils/Mailers/sendWelcomeEmail.js
+++ utils/Mailers/sendWelcomeEmail.js
@@ -1,70 +1,66 @@
-import nodemailer from 'nodemailer'
-import capitalize from '../capitalize';
+import nodemailer from "nodemailer";
+import capitalize from "../capitalize";
 
-import PATHS from '../URLs';
-const { domain, newSpot } = PATHS
-
+import PATHS from "../URLs";
+const { domain, newSpot } = PATHS;
 
 const sendWelcomeEmail = async (userRecipient, userName) => {
-    console.log("userRecipient", userRecipient)
-    console.log("userName", userName)
+  console.log("userRecipient", userRecipient);
+  console.log("userName", userName);
 
-    if (!userRecipient || !userName) {
-        return { success: false, result: `At least one parameter to send  the email is missing [sendWelcomeEmail]` }
-    }
+  if (!userRecipient || !userName) {
+    return {
+      success: false,
+      result: `At least one parameter to send  the email is missing [sendWelcomeEmail]`,
+    };
+  }
 
+  try {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GOOGLE_USER,
+        pass: process.env.GOOGLE_PASSWORD,
+      },
+      port: 587,
+      secure: false, // true for 465, false for other ports
+    });
 
-    try {
+    // Only tests connection and authentication but it does not check if the service allows you to use a specific envelope From address or not.
+    transporter.verify((error, success) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
 
-
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.GOOGLE_USER,
-                pass: process.env.GOOGLE_PASSWORD
-            },
-            port: 587,
-            secure: false, // true for 465, false for other ports
-        });
-
-
-        // Only tests connection and authentication but it does not check if the service allows you to use a specific envelope From address or not.
-        transporter.verify((error, success) => {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log("Server is ready to take our messages");
-            }
-        });
-
-
-        const htmlToSend = `
+    const htmlToSend = `
         <h3> Hello ${capitalize(userName)} !  </h3>
         <p> Welcome to the Spot Finder Community!... </p>
         <p> 
         Start <a target = "_" href="${domain}/${newSpot}"> adding new spots here
         </a> or <a target = "_" href="${domain}"> browse through our amazing existing spots </a> already shared by our community!
         </p>
-        <p> Thank you</p>`
+        <p> Thank you</p>`;
 
+    // send mail with defined transport object
+    const mailOptions = await transporter.sendMail({
+      from: "Spot Finder team 👻 <process.env.GOOGLE_USER>", // sender name + address
+      to: userRecipient,
+      subject: `${capitalize(userName)}, Welcome to Spot Finder! ✔ !`, // Subject line
+      text: "Hello world?", // plain text body
+      html: htmlToSend, // html body
+    });
 
-        // send mail with defined transport object
-        const mailOptions = await transporter.sendMail({
-            from: 'Spot Finder team 👻 <process.env.GOOGLE_USER>', // sender name + address
-            to: userRecipient,
-            subject: `${capitalize(userName)}, Welcome to Spot Finder! ✔ !`, // Subject line
-            text: "Hello world?", // plain text body
-            html: htmlToSend, // html body
-        });
+    return { success: true, result: `Welcome email sent!` };
+  } catch (error) {
+    return {
+      success: false,
+      result: `There has been an error in sending the welcome email: ${error.stack}`,
+    };
+  }
+};
 
-        return { success: true, result: `Welcome email sent!` }
-
-    } catch (error) {
-        return { success: false, result: `There has been an error in sending the welcome email: ${error.stack}` }
-    }
-
-}
-
-
-export default sendWelcomeEmail
+export default sendWelcomeEmail;
