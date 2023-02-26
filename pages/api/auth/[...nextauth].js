c4c452dfada227b5d5b5362c238a431a7832ce94
Zach <zachariedupain@hotmail.fr01~>
2023-02-26 11:25:45 +0400

    fix: test add vercel env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,6 +15,9 @@ import { compare } from "bcryptjs";
 import sendWelcomeEmail from "../../../utils/Mailers/sendWelcomeEmail";
 import isUserVerified from "../../../utils/Auth/isUserVerified";
 
+console.log("VERCEL_ENV", VERCEL_ENV);
+console.log("VERCEL_URL", VERCEL_URL);
+
 // Authentication logic
 // Added id because : https://stackoverflow.com/questions/69424685/custom-sign-in-page-not-redirecting-correctly-in-next-auth#:~:text=I%20found%20the%20solution%2C%20but%20I%20think%20the%20documentation%20is%20misleading.
 const cred = CredentialsProvider({
