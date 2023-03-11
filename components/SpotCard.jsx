616a795e01e1804f9f161865324acaf843cc659c
Zach <zachariedupain@hotmail.fr01~>
2023-03-11 18:21:09 +0400

    fix tst perf

--- components/SpotCard.jsx
+++ components/SpotCard.jsx
@@ -1,38 +1,38 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
-import Image from "next/image";
-import imageTesting from "../public/imageTesting.jpg";
+import Image from 'next/image'
+import imageTesting from '../public/imageTesting.webp'
 
 const SpotCard = ({ rate, title, description, id, categories, author }) => {
-  const router = useRouter();
+    const router = useRouter()
 
-  const clickDetailsHandler = () => {
-    router.push(`/spots/${id}`);
-  };
+    const clickDetailsHandler = () => {
+        router.push(`/spots/${id}`)
+    }
 
-  return (
-    <>
-      <div onClick={clickDetailsHandler} className="mb-6 cursor-pointer">
-        <div className="w-48">
-          <Image
-            src={imageTesting}
-            alt="Picture"
-            placeholder="blur"
-            className=" rounded-lg"
-          />
-        </div>
+    return (
+        <>
+            <div onClick={clickDetailsHandler} className="mb-6 cursor-pointer">
+                <div className="w-48">
+                    <Image
+                        src={imageTesting}
+                        alt="Picture"
+                        placeholder="blur"
+                        className=" rounded-lg"
+                    />
+                </div>
 
-        <div>
-          <p className="font-semibold">{title}</p>
-          <p className="font-light">{description}</p>
-          <p className="font-light">Shared by {author}</p>
-          <p className="font-light">Grade: {rate}</p>
-          <p className="font-light">{categories.join(", ")}</p>
-        </div>
-      </div>
-    </>
-  );
-};
+                <div>
+                    <p className="font-semibold">{title}</p>
+                    <p className="font-light">{description}</p>
+                    <p className="font-light">Shared by {author}</p>
+                    <p className="font-light">Grade: {rate}</p>
+                    <p className="font-light">{categories.join(', ')}</p>
+                </div>
+            </div>
+        </>
+    )
+}
 
-export default SpotCard;
+export default SpotCard
