a8a57da0c242a5edff5a90949a99f0e93c98f8f1
Zach <zachariedupain@hotmail.fr01~>
2023-03-11 18:34:08 +0400

    public/imageTesting.jpg

--- components/Maps/MapShow.jsx
+++ components/Maps/MapShow.jsx
@@ -1,76 +1,65 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-import { Skeleton } from "@mui/material";
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-import "mapbox-gl/dist/mapbox-gl.css";
-
-import Pin from "./Pin";
+import Pin from './Pin'
 
 import Map, {
-  Marker,
-  NavigationControl,
-  FullscreenControl,
-  ScaleControl,
-  GeolocateControl,
-  useControl,
-} from "react-map-gl";
+    Marker,
+    NavigationControl,
+    FullscreenControl,
+    ScaleControl,
+    GeolocateControl,
+    useControl,
+} from 'react-map-gl'
 
 const MapShow = ({ markerCoordinates }) => {
-  const [isMapLoading, setIsMapLoading] = useState(true);
-  const [aaa, setAAA] = useState(false);
+    const [isMapLoading, setIsMapLoading] = useState(true)
+    const [aaa, setAAA] = useState(false)
 
-  const onLoad = (arg) => {
-    setIsMapLoading(arg.fullyLoaded);
-  };
+    const onLoad = arg => {
+        setIsMapLoading(arg.fullyLoaded)
+    }
 
-  return (
-    <>
-      <div
-        className={`justify-center mt-6 ${isMapLoading ? "flex" : "hidden"}`}
-      >
-        <Skeleton
-          animation="wave"
-          variant="rectangular"
-          width={700}
-          height={500}
-        />
-      </div>
+    return (
+        <>
+            <div className={`justify-center mt-6 ${isMapLoading ? 'flex' : 'hidden'}`}>
+                <h3>Map Loading...</h3>
+            </div>
 
-      <div
-        className={`justify-center mt-6 ${isMapLoading ? "invisible" : "flex"}`}
-      >
-        <Map
-          initialViewState={{
-            latitude: markerCoordinates.Latitude,
-            longitude: markerCoordinates.Longitude,
-            zoom: 3,
-          }}
-          style={{ width: 700, height: 500 }}
-          // mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
-          mapStyle="mapbox://styles/zacharie123dxb/clcnu76zp000514pf3jx9c20g"
-          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-          attributionControl={false}
-          // onLoad={() => setIsMapLoading(false)}
-          onLoad={onLoad}
-        >
-          <Marker
-            longitude={markerCoordinates.Longitude}
-            latitude={markerCoordinates.Latitude}
-            color="red"
-          >
-            <Pin size={20} />
-          </Marker>
+            <div className={`justify-center mt-6 ${isMapLoading ? 'invisible' : 'flex'}`}>
+                <Map
+                    initialViewState={{
+                        latitude: markerCoordinates.Latitude,
+                        longitude: markerCoordinates.Longitude,
+                        zoom: 3,
+                    }}
+                    style={{ width: 700, height: 500 }}
+                    // mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
+                    mapStyle="mapbox://styles/zacharie123dxb/clcnu76zp000514pf3jx9c20g"
+                    mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+                    attributionControl={false}
+                    // onLoad={() => setIsMapLoading(false)}
+                    onLoad={onLoad}
+                >
+                    <Marker
+                        longitude={markerCoordinates.Longitude}
+                        latitude={markerCoordinates.Latitude}
+                        color="red"
+                    >
+                        <Pin size={20} />
+                    </Marker>
 
-          <FullscreenControl />
-          <GeolocateControl />
-          <NavigationControl />
-          <ScaleControl />
-        </Map>
-      </div>
-    </>
-  );
-};
+                    <FullscreenControl />
+                    <GeolocateControl />
+                    <NavigationControl />
+                    <ScaleControl />
+                </Map>
+            </div>
+        </>
+    )
+}
 
-export default MapShow;
+export default MapShow
 
 // <AttributionControl /> credits etc on the map, can custom it
