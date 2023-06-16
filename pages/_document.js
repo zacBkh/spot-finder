b8556e350dcc1ceffbebe1cda12cda40d8081fe0
Zach <zachariedupain@hotmail.fr>
2023-06-16 07:59:05 +0400

    fix: added link to favicon

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,8 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
                     rel="stylesheet"
                 />
+
+                <link rel="icon" href="/favicon.ico" sizes="any" />
             </Head>
             <body>
                 <Main />
