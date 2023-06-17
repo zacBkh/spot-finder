e6e0bd9bb662f04d33327524f3415e493b56e219
Zach <zachariedupain@hotmail.fr>
2023-06-17 17:35:12 +0400

    feat: lazy loaded side drawer filters

---
+++ components/filters-drawer/drawer-logic-wrapper.jsx
@@ -0,0 +1,181 @@
+import { useEffect, useContext } from 'react'
+
+import OpenedDrawer from './opened-drawer'
+import ClosedDrawer from './closed-drawer'
+
+import { SearchBarContext } from '../../context/AppContext'
+
+import getAvrgGrade from '../../utils/get-average-rate'
+
+const SideDrawerFilters = ({
+    onDrawerToggle,
+    isDrawerOpen,
+
+    activeCategories,
+    activeRegion,
+    activeSortCriteria,
+
+    onCatChange,
+    onRegionChange,
+    onSortCriteriaChange,
+
+    allSpots,
+    onFilteredSpotsChange,
+    filteredSpots,
+}) => {
+    // Context API holding value from navbar search input + method to add
+    const searchContext = useContext(SearchBarContext)
+
+    // Maintain the current activee categorie(s) array
+    const catFilterHandler = filteredCat => {
+        if (activeCategories.includes(filteredCat)) {
+            onCatChange(activeCategories.filter(x => x !== filteredCat)) // if already in array -> remove
+        } else {
+            // if NOT already in array -> add
+            onCatChange([...activeCategories, filteredCat])
+        }
+    }
+
+    const regionFilterHandler = filteredRegion => {
+        if (activeRegion === filteredRegion) {
+            onRegionChange('')
+        } else {
+            onRegionChange(filteredRegion)
+        }
+    }
+
+    const addSortHandler = sortingCriteria => {
+        if (activeSortCriteria === sortingCriteria) {
+            onSortCriteriaChange('')
+        } else {
+            onSortCriteriaChange(sortingCriteria)
+        }
+    }
+
+    // Handles cat & region filters
+    useEffect(() => {
+        // If categories + region --> double filter
+        if (activeCategories.length && activeRegion) {
+            onFilteredSpotsChange(
+                allSpots
+                    .filter(spot =>
+                        spot.categories.some(x => activeCategories.includes(x)),
+                    )
+                    .filter(spot => spot.country.region === activeRegion),
+            )
+            return
+        }
+
+        // If categories only
+        if (activeCategories.length) {
+            onFilteredSpotsChange(
+                allSpots.filter(spot =>
+                    spot.categories.some(x => activeCategories.includes(x)),
+                ),
+            )
+            return
+        }
+
+        // If region only
+        if (activeRegion) {
+            onFilteredSpotsChange(
+                allSpots.filter(spot => spot.country.region === activeRegion),
+            )
+            return
+        }
+
+        if (
+            !activeCategories.length &&
+            !activeRegion.length &&
+            !searchContext.value.length
+        ) {
+            onFilteredSpotsChange(allSpots)
+        }
+    }, [activeCategories, activeRegion, searchContext.value.length, allSpots])
+
+    // Handles criteria sorting change
+    useEffect(() => {
+        if (activeSortCriteria) {
+            switch (activeSortCriteria) {
+                case 'Number of visits':
+                    onFilteredSpotsChange(
+                        [...filteredSpots].sort(
+                            // .sort returns same array so we need to mutate it
+                            (a, b) => b.visitors.length - a.visitors.length,
+                        ),
+                    )
+                    break
+                case 'Grade':
+                    onFilteredSpotsChange(
+                        [...filteredSpots].sort(
+                            (a, b) => getAvrgGrade(b.reviews) - getAvrgGrade(a.reviews),
+                        ),
+                    )
+                    break
+
+                case 'Recent':
+                    onFilteredSpotsChange(
+                        [...filteredSpots].sort(
+                            (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+                        ),
+                    )
+                    break
+            }
+            return
+        }
+
+        if (!activeSortCriteria) {
+            onSortCriteriaChange(allSpots)
+        }
+    }, [activeSortCriteria])
+
+    // If search bar only, disable all other filters
+    useEffect(() => {
+        if (searchContext.value.length) {
+            onCatChange([])
+            onRegionChange('')
+            onSortCriteriaChange('')
+
+            onFilteredSpotsChange(
+                allSpots.filter(spot =>
+                    spot.title.toLowerCase().includes(searchContext.value.toLowerCase()),
+                ),
+            )
+            return
+        }
+
+        onFilteredSpotsChange(allSpots)
+    }, [searchContext.value.length, searchContext.value, allSpots])
+
+    return (
+        <>
+            <aside
+                className={`${
+                    isDrawerOpen ? 'md:w-[30%] 2xl:w-[20%]' : 'md:w-[8%] xl:w-[5%]'
+                } w-full transition-all
+                    flex flex-col items-center border-r-[1px] border-[#cfd9e0] py-2 md:sticky md:top-28  px-1`}
+            >
+                <div className="w-full">
+                    {isDrawerOpen ? (
+                        <OpenedDrawer
+                            activeCategories={activeCategories}
+                            activeRegion={activeRegion}
+                            onClickFilterCat={catFilterHandler}
+                            onClickFilterRegion={regionFilterHandler}
+                            onAddSort={addSortHandler}
+                            activeCriteria={activeSortCriteria}
+                            onDrawerToggle={() => onDrawerToggle()}
+                        />
+                    ) : (
+                        <ClosedDrawer
+                            activeFilters={{ activeCategories, activeRegion }}
+                            onDrawerToggle={() => onDrawerToggle()}
+                        />
+                    )}
+                </div>
+            </aside>
+        </>
+    )
+}
+
+export default SideDrawerFilters
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,12 @@
-import { useState, useEffect, useContext } from 'react'
-
-import { SearchBarContext } from '../context/AppContext'
+import { useState } from 'react'
 
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 
 import { GETSpotFetcherAll } from '../services/fetchers-ssr'
 
-import getAvrgGrade from '../utils/get-average-rate'
-
 import ToggleToMapView from '../components/toggle-to-map-view-btn'
 import SpotCard from '../components/spot-index-card'
-import ClosedDrawer from '../components/filters-drawer/closed-drawer'
-import OpenedDrawer from '../components/filters-drawer/opened-drawer'
 
 export const getServerSideProps = async context => {
     try {
@@ -39,9 +33,6 @@ export const getServerSideProps = async context => {
 }
 
 const AllSpots = ({ spots }) => {
-    // Context API holding value from navbar search input + method to add
-    const searchContext = useContext(SearchBarContext)
-
     const [activeCategories, setActiveCategories] = useState([])
     const [activeRegion, setActiveRegion] = useState('')
     const [activeSortCriteria, setactiveSortCriteria] = useState('')
@@ -49,132 +40,11 @@ const AllSpots = ({ spots }) => {
     const [filteredSpots, setFilteredSpots] = useState(spots)
 
     const [isOnMapMode, setIsOnMapMode] = useState(false)
-    const toggleToMapViewHandler = () => {
-        setIsOnMapMode(prevMapState => !prevMapState)
-    }
-
-    // Maintain the current activee categorie(s) array
-    const catFilterHandler = filteredCat => {
-        if (activeCategories.includes(filteredCat)) {
-            setActiveCategories(prevState => prevState.filter(x => x !== filteredCat)) // if already in array -> remove
-        } else {
-            // if NOT already in array -> add
-            setActiveCategories(prevState => [...prevState, filteredCat])
-        }
-    }
-
-    const regionFilterHandler = filteredRegion => {
-        if (activeRegion === filteredRegion) {
-            setActiveRegion('')
-        } else {
-            setActiveRegion(filteredRegion)
-        }
-    }
-
-    const addSortHandler = sortingCriteria => {
-        if (activeSortCriteria === sortingCriteria) {
-            setactiveSortCriteria('')
-        } else {
-            setactiveSortCriteria(sortingCriteria)
-        }
-    }
-
-    // Handles cat & region filters
-    useEffect(() => {
-        // If categories + region --> double filter
-        if (activeCategories.length && activeRegion) {
-            setFilteredSpots(
-                spots
-                    .filter(spot =>
-                        spot.categories.some(x => activeCategories.includes(x)),
-                    )
-                    .filter(spot => spot.country.region === activeRegion),
-            )
-            return
-        }
-
-        // If categories only
-        if (activeCategories.length) {
-            setFilteredSpots(
-                spots.filter(spot =>
-                    spot.categories.some(x => activeCategories.includes(x)),
-                ),
-            )
-            return
-        }
-
-        // If region only
-        if (activeRegion) {
-            setFilteredSpots(spots.filter(spot => spot.country.region === activeRegion))
-            return
-        }
-
-        if (
-            !activeCategories.length &&
-            !activeRegion.length &&
-            !searchContext.value.length
-        ) {
-            setFilteredSpots(spots)
-        }
-    }, [activeCategories, activeRegion, searchContext.value.length, spots])
-
-    useEffect(() => {
-        if (activeSortCriteria) {
-            switch (activeSortCriteria) {
-                case 'Number of visits':
-                    setFilteredSpots(
-                        [...filteredSpots].sort(
-                            // .sort returns same array so we need to mutate it
-                            (a, b) => b.visitors.length - a.visitors.length,
-                        ),
-                    )
-                    break
-                case 'Grade':
-                    setFilteredSpots(
-                        [...filteredSpots].sort(
-                            (a, b) => getAvrgGrade(b.reviews) - getAvrgGrade(a.reviews),
-                        ),
-                    )
-                    break
-
-                case 'Recent':
-                    setFilteredSpots(
-                        [...filteredSpots].sort(
-                            (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
-                        ),
-                    )
-                    break
-            }
-            return
-        }
-
-        if (!activeSortCriteria) {
-            setFilteredSpots(spots)
-        }
-    }, [activeSortCriteria])
-
-    // If search bar only, disable all other filters
-    useEffect(() => {
-        if (searchContext.value.length) {
-            setActiveCategories([])
-            setActiveRegion('')
-            setactiveSortCriteria('')
-
-            setFilteredSpots(
-                spots.filter(spot =>
-                    spot.title.toLowerCase().includes(searchContext.value.toLowerCase()),
-                ),
-            )
-            return
-        }
-
-        setFilteredSpots(spots)
-    }, [searchContext.value.length, searchContext.value, spots])
 
     const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
-    const drawerToggleHandler = () => {
-        setIsDrawerOpen(prev => !prev)
+    const toggleToMapViewHandler = () => {
+        setIsOnMapMode(prevMapState => !prevMapState)
     }
 
     const initialMapCoordinates = {
@@ -200,6 +70,10 @@ const AllSpots = ({ spots }) => {
         features: arrayOfSpotsGEOJSON,
     }
 
+    const drawerToggleHandler = () => {
+        setIsDrawerOpen(prev => !prev)
+    }
+
     const DynamicMapIndex = dynamic(
         () =>
             import(
@@ -212,6 +86,18 @@ const AllSpots = ({ spots }) => {
         },
     )
 
+    const DynamicDrawer = dynamic(
+        () =>
+            import(
+                /* webpackChunkName: 'lazy-loaded-dynamic-side-drawer-' */
+                '../components/filters-drawer/drawer-logic-wrapper'
+            ),
+        {
+            ssr: false,
+            loading: () => <div className="md:w-[8%] xl:w-[5%]">Loading Drawer...</div>,
+        },
+    )
+
     return (
         <>
             <Head>
@@ -220,31 +106,24 @@ const AllSpots = ({ spots }) => {
             </Head>
 
             <div className="flex flex-col md:flex-row gap-x-4 gap-y-4">
-                <aside
-                    className={`${
-                        isDrawerOpen ? 'md:w-[30%] 2xl:w-[20%]' : 'md:w-[8%] xl:w-[5%]'
-                    } w-full transition-all
-                    flex flex-col items-center border-r-[1px] border-[#cfd9e0] py-2 md:sticky md:top-28  px-1`}
-                >
-                    <div className="w-full">
-                        {isDrawerOpen ? (
-                            <OpenedDrawer
-                                activeCategories={activeCategories}
-                                activeRegion={activeRegion}
-                                onClickFilterCat={catFilterHandler}
-                                onClickFilterRegion={regionFilterHandler}
-                                onAddSort={addSortHandler}
-                                activeCriteria={activeSortCriteria}
-                                onDrawerToggle={drawerToggleHandler}
-                            />
-                        ) : (
-                            <ClosedDrawer
-                                activeFilters={{ activeCategories, activeRegion }}
-                                onDrawerToggle={drawerToggleHandler}
-                            />
-                        )}
-                    </div>
-                </aside>
+                <DynamicDrawer
+                    onDrawerToggle={drawerToggleHandler}
+                    isDrawerOpen={isDrawerOpen}
+                    activeCategories={activeCategories}
+                    activeRegion={activeRegion}
+                    activeSortCriteria={activeSortCriteria}
+                    onCatChange={newActiveCat => setActiveCategories(newActiveCat)}
+                    onRegionChange={newActiveRegion => setActiveRegion(newActiveRegion)}
+                    onSortCriteriaChange={newSortCriteria =>
+                        setactiveSortCriteria(newSortCriteria)
+                    }
+                    allSpots={spots}
+                    onFilteredSpotsChange={newFilteredSpots =>
+                        setFilteredSpots(newFilteredSpots)
+                    }
+                    filteredSpots={filteredSpots}
+                />
+
                 {isOnMapMode ? (
                     <div className="w-screen h-[87vh]  ">
                         <DynamicMapIndex
