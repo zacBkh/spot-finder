533360ec8bbade47b0b040d072ae4833aa2bbb24
Zach <zachariedupain@hotmail.fr>
2023-07-02 11:32:38 +0400

    fix: add disabled btn state in modal deletion spot

--- components/modals/modals-deletion-layout.jsx
+++ components/modals/modals-deletion-layout.jsx
@@ -1,5 +1,9 @@
+import { useState } from 'react'
+
 import { RiErrorWarningLine } from 'react-icons/ri'
 import { AiOutlineClose } from 'react-icons/ai'
+
+import { DISABLED_STYLE } from '../../constants/disabled-style'
 
 const LayoutModalDeletion = ({
     onCloseModal,
@@ -14,6 +18,12 @@ const LayoutModalDeletion = ({
         if (shouldTriggerModalClose) {
             onCloseModal()
         }
+    }
+
+    const [shouldModalLoad, setShouldModalLoad] = useState(false)
+    const confirmDeletionHandler = () => {
+        setShouldModalLoad(true)
+        onConfirmedAction()
     }
     return (
         <>
@@ -37,17 +47,19 @@ const LayoutModalDeletion = ({
                         <h3 className="text-lg">{text}</h3>
                         <h4 className="mb-5 text-lg font-semibold">{text2}</h4>
                         <button
+                            disabled={shouldModalLoad}
                             aria-label={btnConfirm}
-                            onClick={onConfirmedAction}
-                            className="text-white bg-primary hover:bg-primary-hov rounded-lg inline-flex items-center px-5 py-2.5 text-center mr-2 mb-2 sm:mb-0"
+                            onClick={confirmDeletionHandler}
+                            className={`${DISABLED_STYLE} text-white bg-primary hover:bg-primary-hov rounded-lg inline-flex items-center px-5 py-2.5 text-center mr-2 mb-2 sm:mb-0`}
                         >
                             {btnConfirm}
                         </button>
 
                         <button
+                            disabled={shouldModalLoad}
                             aria-label={btnCancel}
                             onClick={onCloseModal}
-                            className="text-gray-500 bg-white hover:bg-gray-100 rounded-lg border border-gray-200 px-5 py-2.5 hover:text-gray-900 "
+                            className={`${DISABLED_STYLE} text-gray-500 bg-white hover:bg-gray-100 rounded-lg border border-gray-200 px-5 py-2.5 hover:text-gray-900`}
                         >
                             {btnCancel}
                         </button>
