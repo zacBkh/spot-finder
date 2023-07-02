84e9812b71ab481afd8154f7e835225ae931fc47
Zach <zachariedupain@hotmail.fr>
2023-07-02 16:16:11 +0400

    fix: try fix on demand revalid

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,5 +1,6 @@
 const RevalidationOnDemand = async (req, res) => {
     const { secret, pathToRevalidate } = req.query
+    console.log('pathToRevalidate', pathToRevalidate)
 
     // Check for token
     if (secret !== process.env.REVALIDATION_ROUTE_TOKEN) {
@@ -7,12 +8,14 @@ const RevalidationOnDemand = async (req, res) => {
     }
 
     try {
-        await res.revalidate(pathToRevalidate)
+        await res.revalidate('/')
+        // await res.revalidate(pathToRevalidate)
         return res.status(200).json({ revalidated: true })
     } catch (err) {
         // If there was an error, Next.js will continue
         // to show the last successfully generated page
-        return res.status(500).send('Error revalidating')
+        console.log('revalidation error', err)
+        return res.status(500).send(`Error revalidating ${err}`)
     }
 }
 
